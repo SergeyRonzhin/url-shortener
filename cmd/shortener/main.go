9aa96d2265bd8aeb7aef15a27e50975ff573c248
Sergei Ronzhin <ronzhin@mego.travel>
2025-03-22 00:07:20 +0300

    + run service

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func main() {
+	runService()
+}
+
+func runService() {
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		panic(err.Error())
+	}
+}
