58821307d657a5984f56db56cc37d365774063f7
Sergei Ronzhin <ronzhin@mego.travel>
2025-03-22 00:07:36 +0300

    + get and post handlers

---
+++ internal/app/handlers/handlers.go
@@ -0,0 +1,13 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+func GETHandler(rw http.ResponseWriter, rq *http.Request) {
+
+}
+
+func POSTHandler(rw http.ResponseWriter, rq *http.Request) {
+
+}
