9d5413c492be777e8b2538e2e4b3e0ddba46f674
Sergei Ronzhin <ronzhin@mego.travel>
2025-03-22 00:57:02 +0300

    + handling of requests
     + check http method and id param

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"net/http"
+	"strings"
+
+	"github.com/SergeyRonzhin/url-shortener/internal/app/handlers"
 )
 
 func main() {
@@ -9,9 +12,30 @@ func main() {
 }
 
 func runService() {
-	err := http.ListenAndServe(":8080", nil)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", mainHandler)
+
+	err := http.ListenAndServe(":8080", mux)
 
 	if err != nil {
 		panic(err.Error())
 	}
+}
+
+func mainHandler(rw http.ResponseWriter, rq *http.Request) {
+
+	id := strings.TrimPrefix(rq.URL.Path, "/")
+
+	if id == "" && rq.Method == http.MethodPost {
+		handlers.POSTHandler(rw, rq)
+		return
+	}
+
+	if id != "" && rq.Method == http.MethodGet {
+		handlers.POSTHandler(rw, rq)
+		return
+	}
+
+	http.Error(rw, "Bad request", http.StatusBadRequest)
 }
